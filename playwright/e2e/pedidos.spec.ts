1a3a741f5667b4bb5e568cdbc6fce0ef9e318fdf
Thiago Folly <thiagofolly@gmail.com>
2026-05-05 21:11:02 -0300

    teste consulta pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+    await page.getByTestId('search-order-id').fill('VLO-0OHIUM');
+
+    await page.getByTestId('search-order-button').click();
+    await expect(page.getByTestId('order-result-id')).toBeVisible();
+    await expect(page.getByTestId('order-result-id')).toContainText('VLO-0OHIUM');
+    await expect(page.getByTestId('order-result-status')).toBeVisible();
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
