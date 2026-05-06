4c9d00dd61e3efedf7859c243827ee843d6a14ba
Thiago Folly <thiagofolly@gmail.com>
2026-05-05 22:07:26 -0300

    AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+/// AAA - Arrange, Act, Assert
 
+test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
+    await page.goto('http://localhost:5173/'); 
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');   
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+    // Act
     await page.getByTestId('search-order-id').fill('VLO-0OHIUM');
-
     await page.getByTestId('search-order-button').click();
+        
+    // Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible();
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-0OHIUM');
     await expect(page.getByTestId('order-result-status')).toBeVisible();
